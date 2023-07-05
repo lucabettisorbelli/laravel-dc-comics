4044d5b73b1baa2bd15f7366dcdefacd91f66144
luca betti sorbelli <luca betti sorbelli>
2023-07-05 09:19:15 +0200

    bootstrap

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,17 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="..resources/scss/app.scss">
     <title>Movies</title>
 </head>
 <body>
-    <h1>Movies-Dc-Comics</h1>
+    <h1 class="text-center">Movies-Dc-Comics</h1>
     
-    <div>
-        <ul>
-            @foreach ($movies as $movie)
-                <li><a href="{{ route("movies.show", $movie->id) }}">{{$movie->title}}</a></li>
-            @endforeach
-        </ul>
+    <div class="container">
+        <div class="card text-center">
+            <ul>
+                @foreach ($movies as $movie)
+                    <li><a href="{{ route("movies.show", $movie->id) }}">{{$movie->title}}</a></li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 </body>
 </html>
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -4,18 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
     <title>dettaglio film</title>
 </head>
 <body>
-    <h1>Dettaglio film</h1>
-    <h4><a href="{{route("home")}}">Torna alla home</a></h4>
-    <h2><strong>Titolo: </strong>{{$movie->title}}</h2>
-    <h3><strong>Descrizione: </strong>{{$movie->description}}</h3>
-    <h3><strong>Prezzo: </strong>{{$movie->price}}</h3>
-    <h3><strong>Series: </strong>{{$movie->series}}</h3>
-    <h3><strong>Tipo: </strong>{{$movie->type}}</h3>
-    <h3><strong>Artista: </strong>{{$movie->artists}}</h3>
-    <h3><strong>Scrittori: </strong>{{$movie->writers}}</h3>
-    <img src="{{$movie->thumb}}" alt="">
+    <div class="title text-center">
+        <h1>Dettaglio film</h1>
+        <h4><a href="{{route("home")}}">Torna alla home</a></h4>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <div class="card">
+                    <h2><strong>Titolo: </strong>{{$movie->title}}</h2>
+                    <h3><strong>Descrizione: </strong>{{$movie->description}}</h3>
+                    <h3><strong>Prezzo: </strong>{{$movie->price}}</h3>
+                    <h3><strong>Series: </strong>{{$movie->series}}</h3>
+                    <h3><strong>Tipo: </strong>{{$movie->type}}</h3>
+                    <h3><strong>Artista: </strong>{{$movie->artists}}</h3>
+                    <h3><strong>Scrittori: </strong>{{$movie->writers}}</h3>
+                    <div class="boxImage">
+                        <img src="{{$movie->thumb}}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
